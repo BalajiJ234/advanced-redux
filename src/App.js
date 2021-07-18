a6343919f8f02a1996e6d7c247637a9e17873ef3
Balaji J <balajijanarthanan1997@gmail.com>
2021-07-18 15:10:00 +0530

    updates - cart actions

--- src/App.js
+++ src/App.js
@@ -16,13 +16,16 @@ function App() {
   useEffect(() => {
     dispatch(fetchCartData());
   }, [dispatch]);
+
   useEffect(() => {
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    dispatch(sendCartData(cart));
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
   }, [cart, dispatch]);
 
   return (
